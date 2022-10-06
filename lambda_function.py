e9645599b3beed53a51d97cd96743943cc623876
Mustafa1845 <mustafa@irenictech.com>
2022-10-06 18:50:29 +0500

    adas

--- lambda_function.py
+++ lambda_function.py
@@ -27,4 +27,4 @@ def lambda_handler(event, context):
         'statusCode': 200,
         'body': json.dumps('Hello from Lambda, ' + name)
     }
-    # this function is pushed from github and changes made
+    # this function is pushed from github and changes made done
