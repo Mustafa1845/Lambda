74d77548a46ff29d36256be4ee48f42b8183a73b
Mustafa1845 <mustafa@irenictech.com>
2022-10-07 12:22:49 +0500

    now

--- MyFirstFolder/index.js
+++ MyFirstFolder/index.js
@@ -2,7 +2,7 @@ exports.handler = async (event) => {
     // TODO implement
     const response = {
         statusCode: 200,
-        body: JSON.stringify('Hello from Lambda from github actions!'),
+        body: JSON.stringify('Hello from Lambda from github actions! lamdba.zip'),
     };
     return response;
 };
